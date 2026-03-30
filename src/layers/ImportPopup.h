3b401e9c54c5b22fdbde94210a313a58d333e1e3
Fubuki-Hime <tsecheukhim@gmail.com>
2026-03-30 13:30:33 +0800

    Update ImportPopup.h

--- src/layers/ImportPopup.h
+++ src/layers/ImportPopup.h
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
 #include <Geode/ui/TextInput.hpp>
+#include <Geode/ui/FilePickEventFilter.hpp>
 #include <matjson.hpp>
 #include <sstream>
 
@@ -18,18 +19,23 @@ protected:
     CCLabelBMFont* m_fileLabel = nullptr;
     CCLabelBMFont* m_countLabel = nullptr;
     TextInput* m_drawScaleInput = nullptr;
+    EventListener<Task<Result<std::filesystem::path>>::Event> m_pickListener;  // ADDcos2d::CCArray*>, public TextInputDelegate {
+protected:
+    int m_zOrder = 0;
+    int m_objsCount = 0;
+    float m_drawScale = 1;
+    matjson::Value m_jsonSets;
+    std::ostringstream m_objsString;
+    GameObject* m_centerObj = nullptr;
+    TextInput* m_zLayerInput = nullptr;
+    CCLabelBMFont* m_fileLabel = nullptr;
+    CCLabelBMFont* m_countLabel = nullptr;
+    TextInput* m_drawScaleInput = nullptr;
+    EventListener<Task<Result<std::filesystem::path>>::Event> m_pickListener; // ADD THIS LINE
     static constexpr int m_CircleId = 497;
     static constexpr int m_SquareId = 495;
     const std::set<int> m_squareTypes = {0, 1, 2};
-    const std::set<int> m_validTypes = {5, 3, 4, 32, 8, 16}; // 0, 1, 2
+    const std::set<int> m_validTypes = {5, 3, 4, 32, 8, 16};
     static constexpr CCSize m_popupSize = CCSize(385.f, 245.f);
 protected:
-    void parseAndPlace();
-    void importJSON(cocos2d::CCObject* sender);
-    void checkAlert(cocos2d::CCObject* sender);
-    void textChanged(CCTextInputNode *p0) override;
-    bool setup(cocos2d::CCArray* selectedObj) override;
-    void rgbToHsv(float fR, float fG, float fB, float& fH, float& fS, float& fV);
-public:
-    static ImportPopup* create(cocos2d::CCArray* selectedObj);
-};
+    void parse`:**
